701d8165ab28f027f4c881d63f52c772eab57675
wb1110 <strengthcoachwb@gmail.com>
2021-11-03 14:17:17 -0500

    axios done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  
+  const [nasa, setNasa] = useState();
+  const axios = require('axios');
+
+  useEffect(() => {
+  axios.get('https://api.nasa.gov/planetary/apod?api_key=FjrmJzNeziBJDn5r5sMIcFB4fmw3zwqVbo8VrhB7')
+  .then(res => {
+    setNasa(res.data);
+  })
+  .catch(err => {
+    console.log(err)
+  })
+}, [])
+  
   return (
     <div className="App">
       <p>
